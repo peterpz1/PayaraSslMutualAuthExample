df5f9356a548d967fce991b375c287e1bcb8eb03
Piotr Zych <piotr.zych2@orange.com>
2019-06-16 15:03:33 +0200

    proper service url

--- src/main/java/org/peterpz/payara/test/TestService.java
+++ src/main/java/org/peterpz/payara/test/TestService.java
@@ -3,7 +3,7 @@ package org.peterpz.payara.test;
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 
-@WebService(serviceName = "/webservice/soap/TestService")
+@WebService(serviceName = "webservice/soap/TestService")
 public class TestService {
     @WebMethod
     public String hello() {
